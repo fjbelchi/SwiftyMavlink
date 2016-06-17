1e80f996ee6da14b945d863693b1aead62aeb3c7
fjbelchi <fjbelchi@gmail.com>
2016-06-17 18:59:02 +0200

    Untracking .build directory

--- .build/versionData/Socks.swift
+++
@@ -1,3 +0,0 @@
-public let url: String = "https://github.com/czechboy0/Socks.git"
-public let version: (major: Int, minor: Int, patch: Int, prereleaseIdentifiers: [String], buildMetadata: String?) = (0, 7, 0, [], nil)
-public let versionString: String = "0.7.0"
